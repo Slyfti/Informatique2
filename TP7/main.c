2f224e12780b3bef480f6d62c7d38ac1d7fc969c
Capblancq Sylvain <capblancqs@pce10821.etude.pau.eisti.fr>
2023-03-27 16:01:14 +0200

    a

--- TP7/main.c
+++ TP7/main.c
@@ -4,13 +4,14 @@
 #include "trisRapides.h"
 #include <stdlib.h>
 #include <time.h>
-#define N 1000000
+#include <string.h>
+#define N 1000
 int selectTri() {
     int error = 0;
     int in;
     while (!error) {
     
-    printf("Veuillez selectionner le tri : \n 1. Tri Selection \n 2. Tri Bulle \n 3. Tri Insertion \n 4. Tri Rapide \n 5. Tri Fusion \n \n");
+    printf("\n 1. Tri Selection \n 2. Tri Bulle \n 3. Tri Insertion \n 4. Tri Rapide \n 5. Tri Fusion \n \n");
     error = scanf("%d",&in);
     }
     return in;
@@ -24,10 +25,8 @@ int main(){
     initialiser(tab,N);
     // afficher(tab,N);
     printf("\n");
-
+    printf("Veuillez selectionner le premier tri");
     int triId = selectTri();
-    
-    
     clock_t t1,t2;
     float temps_total;
     t1=clock();
@@ -52,6 +51,30 @@ int main(){
     printf("Mon tri numéro %d, a mis %f de secondes à s'executer \n",triId,temps_total);
     printf("\n");
 
+    printf("Veuillez selectionner le second tri");
+    int triId2 = selectTri();
+    t1=clock();
+    switch(triId2) {
+        case 1: triSelection(tab,N);
+        break;
+
+        case 2: triBulles(tab,N);
+        break;
+        
+        case 3: triInsertion(tab,N);
+        break;
+
+        case 4: triRapide(tab,0,N-1);
+        break;
+
+        case 5: triFusion(tab,0,N-1);
+        break;
+    }
+    t2=clock();
+    float temps_total2 = (float)(t2-t1)/CLOCKS_PER_SEC;
+
+    printf("Le tri%d, a mis %f de secondes à s'executer \n",triId,temps_total);
+    printf("Mon tri numéro %d, a mis %f de secondes à s'executer, il est %f fois plus rapide que le premier \n",triId2,temps_total2, temps_total/temps_total2);
     printf("\n");
 
     
